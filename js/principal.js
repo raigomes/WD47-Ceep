0c73dd4fbed50d26dc04748ce59563075fd4f54b
Rai Gomes <raigomes@github.com>
2017-01-16 16:21:04 -0200

    Incluindo funcionalidades de adicionar cartoes com JQuery

--- js/principal.js
+++ js/principal.js
@@ -15,17 +15,52 @@ document.querySelector("#mudaLayout").addEventListener("click", function(){
 
 });
 
-/* Remover Botoes */
+/* Remover Cartoes */
 var botoes = document.querySelectorAll(".botao-remover");
 for (var i = 0; i < botoes.length; i++) {
-	botoes[i].addEventListener("click", removerCartao);
+	botoes[i].addEventListener("click", removeCartao);
 }
 
-function removerCartao () {
+function removeCartao () {
 	var cartao = document.querySelector("#cartao_" + this.dataset.id);
-	console.log(this.dataset.id);
+	console.log(cartao);	
 	cartao.classList.add("cartao--some");
 	setTimeout(function() {
 		cartao.remove();
 	}, 400);
 }
+
+/* Adicionar Cartoes com JQuery */
+var contador = $(".cartao").length;
+$(".novoCartao").submit(function(event){
+	event.preventDefault(); //Previne recarregar a página (comportamento default do form)
+
+	var campoConteudo = $(".novoCartao-conteudo");
+	var conteudo = campoConteudo.val().trim();
+
+	if(conteudo) {
+		contador++;
+
+		//cria o botao de remover
+		var botaoRemove = $("<button>").addClass("opcoesDoCartao-remove")
+									   .attr("data-id", contador)
+									   .text("Remover")
+									   .click(removeCartao);
+
+		//cria a div de opçoes
+		var opcoes = $("<div>").addClass("opcoesDoCartao")
+							   .append(botaoRemove);
+
+		var conteudoTag = $("<p>").addClass("cartao-conteudo")
+								.append(conteudo);
+
+		//Acrescenta o append para colocar a div opcoes no cartao
+		$("<div>").attr("id", "cartao_" + contador)
+				.addClass("cartao")
+				.append(opcoes)
+				.append(conteudoTag)
+				.prependTo(".mural");
+	}
+
+	campoConteudo.val("");	
+});
